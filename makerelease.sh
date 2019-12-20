9ae85bb5aed62e7cfbfe7b574f33ab2c0411031d
Roy Stogner <roy@stogners.org>
2019-12-19 22:37:48 -0600

    Add test/Makefile.in to release tags/branches

--- makerelease.sh
+++ makerelease.sh
@@ -25,7 +25,7 @@ git add Makefile.in aclocal.m4 \
         src/utilities/include/timpi/timpi_config.h.tmp.in build-aux \
         configure m4/libtool.m4 m4/ltoptions.m4 \
         m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 \
-        src/Makefile.in configure.ac
+        src/Makefile.in test/Makefile.in configure.ac
 
 git commit -m "Add bootstrap output"
 
