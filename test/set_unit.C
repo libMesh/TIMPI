996be81378cf78e73bfc9ba450c74b937437ada9
Roy Stogner <roy@stogners.org>
2021-06-01 22:32:30 -0500

    set_union({map,unordered_{set,map}}) unit tests

--- test/set_unit.C
+++ test/set_unit.C
@@ -11,6 +11,37 @@ using namespace TIMPI;
 
 Communicator *TestCommWorld;
 
+void inserter(std::set<int> & s, int i)
+{ s.insert(i); }
+
+void inserter(std::unordered_set<int> & s, int i)
+{ s.insert(i); }
+
+void inserter(std::map<int, int> & m, int i)
+{ m.insert(std::make_pair(i,2*i+3)); }
+
+void inserter(std::unordered_map<int, int> & m, int i)
+{ m.insert(std::make_pair(i,2*i+3)); }
+
+
+void tester(const std::set<int> & s, int i)
+{ TIMPI_UNIT_ASSERT( s.count(i) == std::size_t(1) ); }
+
+void tester(const std::unordered_set<int> & s, int i)
+{ TIMPI_UNIT_ASSERT( s.count(i) == std::size_t(1) ); }
+
+void tester(const std::map<int, int> & m, int i)
+{
+  TIMPI_UNIT_ASSERT( m.count(i) == std::size_t(1) );
+  TIMPI_UNIT_ASSERT( m.at(i) == 2*i+3 );
+}
+
+void tester(const std::unordered_map<int, int> & m, int i)
+{
+  TIMPI_UNIT_ASSERT( m.count(i) == std::size_t(1) );
+  TIMPI_UNIT_ASSERT( m.at(i) == 2*i+3 );
+}
+
 
   template <class Set>
   void testUnion()
@@ -19,18 +50,18 @@ Communicator *TestCommWorld;
 
     const int N = TestCommWorld->size();
 
-    data.insert(TestCommWorld->rank());
-    data.insert(2*N);
-    data.insert(3*N + TestCommWorld->rank());
+    inserter(data, TestCommWorld->rank());
+    inserter(data, 2*N);
+    inserter(data, 3*N + TestCommWorld->rank());
 
     TestCommWorld->set_union(data);
 
     TIMPI_UNIT_ASSERT( data.size() == std::size_t(2*N+1) );
-    TIMPI_UNIT_ASSERT( data.count(2*N) == std::size_t(1) );
+    tester(data, 2*N);
     for (int p=0; p<N; ++p)
       {
-        TIMPI_UNIT_ASSERT( data.count(p) == std::size_t(1) );
-        TIMPI_UNIT_ASSERT( data.count(3*N+p) == std::size_t(1) );
+        tester(data, p);
+        tester(data, 3*N+p);
       }
   }
 
@@ -41,9 +72,9 @@ int main(int argc, const char * const * argv)
   TestCommWorld = &init.comm();
 
   testUnion<std::set<int>>();
-
-  // TODO
-  // testUnion<std::unordered_set<int>>();
+  testUnion<std::unordered_set<int>>();
+  testUnion<std::map<int, int>>();
+  testUnion<std::unordered_map<int, int>>();
 
   return 0;
 }
